1638546c82aab0d739ea8618963e1ce209a63693
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-04 09:57:14 +0800

    feat: add secure headers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
+import { secureHeaders } from 'hono/secure-headers'
 import { useRoutes } from './routes'
 
 const app = new Hono()
 app.use(logger())
+app.use(secureHeaders())
 
 useRoutes(app)
 
