395a1a8bb95c4a948bc377bcafb22fac3e88f1f7
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-04 11:24:55 +0800

    feat: add public route handler

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import type { Hono } from 'hono'
 import { healthRoute } from './health'
+import { staticRoute } from './public'
 
 export function useRoutes(app: Hono) {
   app.route('/', healthRoute)
+  app.route('/', staticRoute)
 
   return app
 }
