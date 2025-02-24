8b9b04c31a0c8cb666d08519cf0735415a0f45b9
Achmad F. Ibrahim <acfatah@gmail.com>
2025-02-25 07:10:42 +0800

    feat: add upload file form and route example

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import type { Hono } from 'hono'
 import { healthRoute } from './health'
 import { staticRoute } from './public'
+import { uploadRoute } from './upload'
 
 export function useRoutes(app: Hono) {
   app.route('/', healthRoute)
   app.route('/', staticRoute)
+  app.route('/', uploadRoute)
 
   return app
 }
