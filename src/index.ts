86f12be197f7cfdbf79725471c2954a43edf4e57
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-06 09:27:56 +0800

    refactor: define 3000 as DEFAULT_PORT constant

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { secureHeaders } from 'hono/secure-headers'
 import process from 'node:process'
 import { useRoutes } from './routing'
 
+export const DEFAULT_PORT = 3000 as const
+
 const app = new Hono()
 app.use(logger())
 app.use(secureHeaders())
@@ -12,5 +14,5 @@ useRoutes(app)
 export default {
   fetch: app.fetch,
   request: app.request,
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || DEFAULT_PORT,
 }
