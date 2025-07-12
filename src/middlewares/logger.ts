ae04185324ce40ac5582a4f2717d2bf637d58740
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-13 06:41:42 +0800

    refactor: add return type for clarity

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,7 +1,8 @@
+import type { MiddlewareHandler } from 'hono'
 import { pinoLogger } from 'hono-pino'
 import { logger as pino } from '@/lib/logger'
 
-export function logger() {
+export function logger(): MiddlewareHandler {
   return pinoLogger({
     pino,
   })
