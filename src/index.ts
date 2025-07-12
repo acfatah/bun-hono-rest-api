910f59f6e82084002919a7c869aa9f3fd02c6779
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-12 20:23:31 +0800

    feat: add hono-pino logger
    https://github.com/maou-shonen/hono-pino

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Hono } from 'hono'
-import { logger } from 'hono/logger'
 import { secureHeaders } from 'hono/secure-headers'
 import process from 'node:process'
 import { migrate } from '@/lib/sqlite-migrator'
+import { logger } from '@/middlewares/logger'
 import { useRoutes } from '@/routing'
 
 const DEFAULT_PORT = 3000
