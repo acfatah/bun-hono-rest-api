2805d16b5c66e7f472a0b327be6fc39676a0dbb3
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-13 20:17:28 +0800

    feat: add drizzle-cuid2

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,7 +1,9 @@
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 import { timestamps } from './helpers/timestamps'
 
-export const users = sqliteTable('users', {
+export const user = sqliteTable('users', {
+  // Optionally use cuid2
+  // id: cuid2('id').defaultRandom().primaryKey(),
   id: integer().primaryKey({ autoIncrement: true }),
   email: text().notNull().unique(),
   ...timestamps,
