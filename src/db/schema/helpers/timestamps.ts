b7d7ca36519d3ae64489582d6ee4e287bfcc9b96
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-15 05:54:03 +0800

    chore: add timestamp example

--- src/db/schema/helpers/timestamps.ts
+++ src/db/schema/helpers/timestamps.ts
@@ -1,8 +1,16 @@
-import { integer } from 'drizzle-orm/sqlite-core'
+import { sql } from 'drizzle-orm'
+import { text } from 'drizzle-orm/sqlite-core'
+// import { integer } from 'drizzle-orm/sqlite-core'
 
 // https://orm.drizzle.team/docs/guides/timestamp-default-value#sqlite
 export const timestamps = {
-  createdAt: integer('created_at', { mode: 'timestamp' }).$defaultFn(() => new Date()).notNull(),
-  updatedAt: integer('updated_at', { mode: 'timestamp' }).$defaultFn(() => new Date()).notNull(),
-  deletedAt: integer('deleted_at'),
+  createdAt: text('created_at').notNull().default(sql`(current_timestamp)`),
+  updatedAt: text('update_at'),
+  deletedAt: text('deleted_at'),
+
+  // Using epoch for timestamps. Needs to be converted to milliseconds
+  // (multiply by 1000) in frontend application code.
+  // createdAt: integer('created_at', { mode: 'timestamp' }).$defaultFn(() => new Date()).notNull(),
+  // updatedAt: integer('updated_at', { mode: 'timestamp' }).$defaultFn(() => new Date()).notNull(),
+  // deletedAt: integer('deleted_at'),
 }
