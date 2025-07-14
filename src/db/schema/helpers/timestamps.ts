16d974e5fc80bd1b382041f5b3fffb6666b30b7d
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-14 10:42:00 +0800

    refactor: update timestamp columns

--- src/db/schema/helpers/timestamps.ts
+++ src/db/schema/helpers/timestamps.ts
@@ -1,11 +1,8 @@
-import { sql } from 'drizzle-orm'
-import { text } from 'drizzle-orm/sqlite-core'
+import { integer } from 'drizzle-orm/sqlite-core'
 
 // https://orm.drizzle.team/docs/guides/timestamp-default-value#sqlite
 export const timestamps = {
-  createdAt: text('created_at')
-    .notNull()
-    .default(sql`(current_timestamp)`),
-  updatedAt: text('update_at'),
-  deletedAt: text('deleted_at'),
+  createdAt: integer('created_at', { mode: 'timestamp' }).$defaultFn(() => new Date()).notNull(),
+  updatedAt: integer('updated_at', { mode: 'timestamp' }).$defaultFn(() => new Date()).notNull(),
+  deletedAt: integer('deleted_at'),
 }
