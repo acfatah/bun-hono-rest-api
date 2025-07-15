be8d53d3215236c41e1c5050b0b41cad24cd3e02
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-15 11:40:09 +0800

    refactor: Fallback to console.log when in the development environment

--- src/db/logger.ts
+++ src/db/logger.ts
@@ -3,11 +3,16 @@ import { DefaultLogger } from 'drizzle-orm/logger'
 import process from 'node:process'
 import { logger as libLogger } from '@/lib/logger'
 
+const DEVELOPMENT_ENVIRONMENT = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
 class CustomLogWriter implements LogWriter {
   write(message: string) {
     if (['debug', 'trace'].includes(process.env.LOG_LEVEL || '')) {
-      console.debug(message)
-      libLogger.debug({ drizzle: message }, 'Drizzle ORM Log')
+      // Fallback to default console log for development
+      if (DEVELOPMENT_ENVIRONMENT)
+        console.debug(message)
+      else
+        libLogger.debug({ drizzle: message }, 'Drizzle ORM Log')
     }
   }
 }
