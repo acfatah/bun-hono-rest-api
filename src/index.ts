45fa36d365166ec0b2a26229d1f7a435d1cbb200
Achmad F. Ibrahim <acfatah@gmail.com>
2025-07-29 13:02:59 +0800

    feat: add example cors origin handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { secureHeaders } from 'hono/secure-headers'
 import process from 'node:process'
+import { handleOrigin } from '@/middlewares/cors-utils'
 import { logger } from '@/middlewares/logger'
 import { useRoutes } from '@/router'
 
@@ -8,6 +10,13 @@ export const DEFAULT_PORT = 3000 as const
 
 const app = new Hono()
 app.use(logger())
+
+app.use('/api/*', cors({
+  origin: handleOrigin,
+  allowMethods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  credentials: true,
+}))
+
 app.use(secureHeaders())
 useRoutes(app)
 
