9126ec3bb028771d94505410c81748eedc077915
Achmad F. Ibrahim <acfatah@gmail.com>
2025-09-14 11:28:10 +0800

    refactor: update logger implementation for simpler environment handling

--- templates/starter/src/middlewares/logger.ts
+++ templates/starter/src/middlewares/logger.ts
@@ -1,8 +1,27 @@
+import type { MiddlewareHandler } from 'hono'
+import type { PinoLogger } from 'hono-pino'
 import { pinoLogger } from 'hono-pino'
-import { logger as pino } from '@/lib/logger'
+import process from 'node:process'
+
+const logLevelOrSilent = process.env.LOG_LEVEL || 'silent'
+const onProduction = process.env.NODE_ENV === 'production'
+const onTest = process.env.NODE_ENV === 'test'
 
 export function logger() {
   return pinoLogger({
-    pino,
-  })
+    pino: {
+      base: null,
+      level: logLevelOrSilent,
+      transport: {
+        target: onProduction || onTest ? 'pino/file' : 'pino/pretty',
+        options: {
+          ...(onProduction)
+            ? { append: true, destination: 'production.log' }
+            : (onTest)
+                ? { append: false, destination: 'test.log' }
+                : { colorEnabled: true }, // development
+        },
+      },
+    },
+  }) as unknown as MiddlewareHandler<{ Variables: { logger: PinoLogger } }>
 }
