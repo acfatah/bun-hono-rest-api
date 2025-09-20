9e3e60b4c866182e6f227c0888907631ecf61aa1
Achmad F. Ibrahim <acfatah@gmail.com>
2025-09-20 12:37:27 +0800

    refactor: update protected route test implementation

--- templates/better-auth/src/router.ts
+++ templates/better-auth/src/router.ts
@@ -3,10 +3,8 @@ import { assetsRoute } from '@/modules/assets/assets.routes'
 import { healthRoute } from '@/modules/health/health.routes'
 import { uploadRoute } from '@/modules/upload/upload.routes'
 
+import { session } from '@/middleware/session'
 import { auth } from '@/modules/auth/auth.provider'
-
-// Example route that require auth
-import { protectedRoute } from '@/routes/protected'
 
 export function useRoutes(app: Hono) {
   app.route('/health', healthRoute)
@@ -21,7 +19,9 @@ export function useRoutes(app: Hono) {
   app.on(['POST', 'GET'], '/api/auth/**', ctx => auth.handler(ctx.req.raw))
 
   // Example signed in routes
-  app.route('/protected', protectedRoute)
+  app.get('/protected', session(), ctx => ctx.json({
+    message: 'This is a protected route',
+  }))
 
   // Serve static files under public directory
   app.route('/', assetsRoute)
