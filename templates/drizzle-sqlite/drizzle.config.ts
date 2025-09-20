fcfe0b011e0ba9f572152b79f46b81c31289851e
Achmad F. Ibrahim <acfatah@gmail.com>
2025-09-20 12:16:02 +0800

    fix: update SQLite path references for consistency in drizzle.config.ts

--- templates/drizzle-sqlite/drizzle.config.ts
+++ templates/drizzle-sqlite/drizzle.config.ts
@@ -3,14 +3,14 @@ import process from 'node:process'
 
 export function resolveSqlitePath() {
   if (['production', 'staging'].includes(process.env.NODE_ENV || '')) {
-    return process.env.SQLITE_DB_PATH || '../../data.sqlite3'
+    return process.env.SQLITE_DB_PATH || './data.sqlite3'
   }
   else if (process.env.NODE_ENV === 'test') {
     return ':memory:'
   }
   else {
     // development
-    return '../../data.development.sqlite3'
+    return './data.development.sqlite3'
   }
 }
 
